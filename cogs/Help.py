355e50eaf39b432f25ce9b93934d7ae8586ad502
Nick Hinds <nicholashinds2@gmail.com>
2023-09-05 16:40:16 -0500

    Updated /help command code

--- cogs/Help.py
+++ cogs/Help.py
@@ -9,16 +9,28 @@ import json
 helpGuide = json.load(open("./cogs/help.json"))
 
 
-def createHelpEmbed(pageNum=0, inline=False, interaction=None):
-    pageNum = pageNum % len(list(helpGuide))
-    pageTitle = list(helpGuide)[pageNum]
-    embed = nextcord.Embed(color=nextcord.Color.blue(), title=pageTitle, timestamp=datetime.datetime.now())
-    for key, val in helpGuide[pageTitle].items():
-        embed.add_field(name=key, value=val, inline=inline)
-        embed.set_footer(text=f"Page {pageNum+1} of {len(list(helpGuide))}")
-        embed.set_thumbnail(url="https://media.tenor.com/nuKGpea_I4gAAAAi/star-platinum-heritage-for-the-future.gif")
-        if interaction is not None:
-            embed.set_author(name=interaction.user.name, icon_url=interaction.user.avatar.url)
+def create_help_embed(page_num=0, interaction=None):
+    page_num = page_num % len(list(helpGuide))
+    page_title = list(helpGuide)[page_num]
+    embed = nextcord.Embed(color=nextcord.Color.blue(), title=page_title, timestamp=datetime.datetime.now())
+    page_description = ""
+    current_field_name = ""
+    for key, val in helpGuide[page_title].items():
+        if "Title:" in key:
+            current_field_name = key[7:]
+        elif "update" in key:
+            embed.add_field(name=current_field_name, value=page_description, inline=False)
+            current_field_name = ""
+            page_description = ""
+        else:
+            page_description += f"**/{key}** - {val}\n"
+
+    embed.add_field(name=current_field_name, value=page_description, inline=False)
+    embed.set_footer(text=f"Page {page_num + 1} of {len(list(helpGuide))}")
+    embed.set_thumbnail(
+        url="https://media.tenor.com/nuKGpea_I4gAAAAi/star-platinum-heritage-for-the-future.gif")
+    if interaction is not None:
+        embed.set_author(name=interaction.user.name, icon_url=interaction.user.avatar.url)
     return embed
 
 
@@ -29,27 +41,27 @@ class Help(commands.Cog):
     @nextcord.slash_command(name="help", description="Receive info on all the commands")
     async def help(self, interaction: Interaction):
         print(f"{datetime.datetime.now()}: {interaction.user.name} sent /help")
-        currentPage = 0
+        current_page = 0
 
-        async def next_callback(interaction2):
-            nonlocal currentPage, sent_msg
-            currentPage += 1
-            await sent_msg.edit(embed=createHelpEmbed(pageNum=currentPage, interaction=interaction), view=myview)
+        async def next_callback(z):
+            nonlocal current_page, sent_msg
+            current_page += 1
+            await sent_msg.edit(embed=create_help_embed(page_num=current_page, interaction=interaction), view=myview)
 
-        async def previous_callback(interaction2):
-            nonlocal currentPage, sent_msg
-            currentPage -= 1
-            await sent_msg.edit(embed=createHelpEmbed(pageNum=currentPage, interaction=interaction), view=myview)
+        async def previous_callback(z):
+            nonlocal current_page, sent_msg
+            current_page -= 1
+            await sent_msg.edit(embed=create_help_embed(page_num=current_page, interaction=interaction), view=myview)
 
-        nextButton = Button(label=">", style=ButtonStyle.blurple)
-        nextButton.callback = next_callback
-        previousButton = Button(label="<", style=ButtonStyle.blurple)
-        previousButton.callback = previous_callback
+        next_button = Button(label=">", style=ButtonStyle.blurple)
+        next_button.callback = next_callback
+        previous_button = Button(label="<", style=ButtonStyle.blurple)
+        previous_button.callback = previous_callback
 
         myview = View(timeout=180)
-        myview.add_item(previousButton)
-        myview.add_item(nextButton)
-        sent_msg = await interaction.send(embed=createHelpEmbed(interaction=interaction), view=myview, ephemeral=True)
+        myview.add_item(previous_button)
+        myview.add_item(next_button)
+        sent_msg = await interaction.send(embed=create_help_embed(interaction=interaction), view=myview, ephemeral=True)
 
 
 def setup(client):
